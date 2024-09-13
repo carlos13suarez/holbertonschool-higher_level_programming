51337065145dc9cd43f317f5c7fe6e7cc12938cf
carlos13suarez <charliesuarez10@gmail.com>
2024-09-12 20:13:11 -0500

    task 1 fix 0: fix display of last digit when number is negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-print(f"Last digit of {number} is {last_digit} and is ", end = "")
+if number >= 0:
+    print(f"Last digit of {number} is {last_digit} and is ", end = "")
+else:
+    print(f"Last digit of {number} is -{last_digit} and is ", end = "")
+
 if last_digit > 5:
     print("greater than 5")
 elif last_digit == 0:
