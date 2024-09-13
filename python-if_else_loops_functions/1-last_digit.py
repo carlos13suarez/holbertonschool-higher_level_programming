ddce6c99b2a91d142bbcec93859ffc953c6d42ef
carlos13suarez <charliesuarez10@gmail.com>
2024-09-12 20:20:45 -0500

    task 1 fix 1: fix case when last digit is negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,5 @@ if last_digit > 5:
     print("greater than 5")
 elif last_digit == 0:
     print("0")
-elif (last_digit < 6) and (last_digit != 0):
+elif ((last_digit < 6) and (last_digit != 0)) or (number < 0):
     print("less than 6 and not 0")
