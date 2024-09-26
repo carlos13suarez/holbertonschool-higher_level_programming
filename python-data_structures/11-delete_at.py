9d701600ccaf15d2640ed6a3a703e85fcffe07aa
carlos13suarez <charliesuarez10@gmail.com>
2024-09-25 21:50:10 -0500

    task 11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=[], idx=0):
+    if idx > (len(my_list) - 1):
+        return my_list
+    for item, value in enumerate(my_list):
+        if item == idx:
+            del my_list[item]
+            return my_list
