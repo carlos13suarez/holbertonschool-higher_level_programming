2e3c4a14cd2ec0909d249756f2da88904b1d673d
carlos13suarez <charliesuarez10@gmail.com>
2024-09-25 21:50:47 -0500

    task 11 fix 0: negative idx

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 
 
 def delete_at(my_list=[], idx=0):
-    if idx > (len(my_list) - 1):
+    if idx > (len(my_list) - 1) or idx < 0:
         return my_list
     for item, value in enumerate(my_list):
         if item == idx:
