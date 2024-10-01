42dc84ea7b9926c9e3fb88239964d82b422d2bd7
carlos13suarez <charliesuarez10@gmail.com>
2024-09-30 21:09:41 -0500

    task 0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+def square_matrix_simple(matrix=[]):
+    rows = len(matrix)
+    columns = len(matrix[0])
+    squared_matrix = [[0 for x in range(columns)] for x in range(rows)]
+    for i in range(rows):
+        for j in range(columns):
+            squared_matrix[i][j] = matrix[i][j]**2
+    return squared_matrix
