62b03548c52cb840ea91dcf57888876dae695ee1
carlos13suarez <charliesuarez10@gmail.com>
2024-10-06 16:01:44 -0500

    task 4 fix 3: remove continue statements

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,15 +7,12 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             quotient = 0
             print("division by 0")
-            continue
         except (ValueError, TypeError):
             quotient = 0
             print("wrong type")
-            continue
         except IndexError:
             quotient = 0
             print("out of range")
-            continue
         finally:
             new_list.append(quotient)
     return new_list
