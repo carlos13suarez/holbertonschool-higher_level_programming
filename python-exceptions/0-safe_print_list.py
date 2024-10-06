624747e60d2cef22e73bb30f61d44a0807366838
carlos13suarez <charliesuarez10@gmail.com>
2024-10-06 14:08:34 -0500

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,8 +6,6 @@ def safe_print_list(my_list=[], x=0):
                 break
             print(v, end="")
         print()
-        return i + 1
-    except IndexError:
-        print("Index is out of range")
+        return i
     except:
         print("Unknown error")
