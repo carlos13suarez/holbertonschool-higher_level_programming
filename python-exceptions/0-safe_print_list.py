ed3d1855e8cedcda0d6d7fb95052410edc3be6a3
carlos13suarez <charliesuarez10@gmail.com>
2024-10-06 14:16:54 -0500

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
-        for i, v in enumerate(my_list):
-            if i == x:
-                break
-            print(v, end="")
-        print()
-        return i
-    except:
-        print("Unknown error")
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
+    print()
+    return count
