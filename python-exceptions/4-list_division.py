45b8733c429cb5c9377b5bf3356106a24508669e
carlos13suarez <charliesuarez10@gmail.com>
2024-10-06 16:00:11 -0500

    task 4 fix 2: create empty list and append quotient

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = [None] * list_length
+    new_list = []
     for i in range(list_length):
         try:
-            new_list[i] = int(my_list_1[i]) / int(my_list_2[i])
+            quotient = int(my_list_1[i]) / int(my_list_2[i])
         except ZeroDivisionError:
-            new_list[i] = 0
+            quotient = 0
             print("division by 0")
             continue
         except (ValueError, TypeError):
-            new_list[i] = 0
+            quotient = 0
             print("wrong type")
             continue
         except IndexError:
-            new_list[i] = 0
+            quotient = 0
             print("out of range")
             continue
         finally:
-            pass
+            new_list.append(quotient)
     return new_list
