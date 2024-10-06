eb2cec9f70fe7cb2be0633baf1a21e9c2ba6ced4
carlos13suarez <charliesuarez10@gmail.com>
2024-10-06 15:53:21 -0500

    task 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = [None] * list_length
+    for i in range(list_length):
+        try:
+            new_list[i] = int(my_list_1[i]) / int(my_list_2[i])
+        except ZeroDivisionError:
+            new_list[i] = 0
+            print("division by 0")
+            continue
+        except (ValueError, TypeError):
+            new_list[i] = 0
+            print("wrong type")
+            continue
+        except IndexError:
+            new_list[i] = 0
+            print("out of range")
+            continue
+    return new_list
