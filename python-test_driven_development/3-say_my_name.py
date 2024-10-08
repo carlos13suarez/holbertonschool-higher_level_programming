038420585b1262b362a7ba854d9b146bc7180755
carlos13suarez <charliesuarez10@gmail.com>
2024-10-07 21:48:16 -0500

    task 2 function documentation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -8,6 +8,20 @@ Functions:
 """
 
 def say_my_name(first_name, last_name=""):
+    """
+    Prints 'My name is <first name> <last name>'
+
+    Arguments:
+        first_name: string of the first name.
+        last_name: string of the last name.
+
+    Raises:
+        TypeError: If first_name is not a string.
+        TypeError: If last_name is not a string.
+
+    Returns:
+        Nothing.
+"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
