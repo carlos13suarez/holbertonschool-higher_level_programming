33503b1175d785f89a673fa64169a1f57b89aeac
carlos13suarez <charliesuarez10@gmail.com>
2024-10-10 20:57:43 -0500

    task 0 fix 1: interchange for and if in my_print

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,11 +45,11 @@ class Square:
         """
         Prints the square taken into account the spaces in position attribute
         """
-        for i in range(self.__position[1]):
-            print()
         if self.__size == 0:
             print()
             return
+        for i in range(self.__position[1]):
+            print()
         for i in range(self.__size):
             for j in range(self.__position[0]):
                 print(" ", end="")
