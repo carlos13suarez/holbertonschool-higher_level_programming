76b7f80aa9c55c36c4c386dc5ec9be7e204e3ddc
carlos13suarez <charliesuarez10@gmail.com>
2024-10-10 20:13:16 -0500

    task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This module defines empty class Square that defines a square"""
+
+
+class Square:
+    """Empty class that defines a Square"""
+    def __init__(self, size=0):
+        """Instation method: create square with a given size attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Returns the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Prints the square"""
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
