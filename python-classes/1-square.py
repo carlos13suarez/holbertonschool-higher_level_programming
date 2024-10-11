9ee1383532622edfa8f6b2469ab469af79a9a6d4
carlos13suarez <charliesuarez10@gmail.com>
2024-10-10 19:37:35 -0500

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines empty class Square that defines a square"""
+
+
+class Square:
+    """Empty class that defines a Square"""
+    def __init__(self, size):
+        """Instation method: create square with a given size attribute"""
+        self.__size = size
