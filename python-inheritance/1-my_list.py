8dcc8ed3deb40c4cf684c7693e315cad95abd57e
carlos13suarez <charliesuarez10@gmail.com>
2024-10-17 20:29:17 -0500

    task 1 fix 0: class documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,7 @@ This module contains the class MyList
 
 
 class MyList(list):
+    """Class MyList that inherits from list class"""
     def print_sorted(self):
         """
         Prints the list, but sorted (ascending sort)
