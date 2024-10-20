7cc429e34a97b18a30a5a5cf1d797cb66f571987
carlos13suarez <charliesuarez10@gmail.com>
2024-10-19 21:27:59 -0500

    task 11 remove super().__init__

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -17,7 +17,6 @@ class Square(Rectangle):
         """
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
 
     def area(self):
         """Returns the area of the square"""
