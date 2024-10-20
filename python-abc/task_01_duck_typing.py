1cecd629c6e76e28b1e9814ac5f8eeeaf7084ed1
carlos13suarez <charliesuarez10@gmail.com>
2024-10-20 16:53:46 -0500

    task 1 docstrings and pycodestyle

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This module contains the Shape abstract class and its subclasses Circle and Rectangle
+"""
 
 
 from abc import ABC, abstractmethod
@@ -6,38 +9,54 @@ from math import pi
 
 
 class Shape(ABC):
+    """Shape abstract class"""
     @abstractmethod
     def area(self):
+        """Area abstract method that must be overridden by its subclasses"""
         pass
 
     @abstractmethod
     def perimeter(self):
+        """Perimeter abstract method that must be overridden by its subclasses"""
         pass
 
 
 class Circle(Shape):
+    """Circle class that inherits from Shape abstract class"""
     def __init__(self, radius):
+        """Circle construct"""
         self.radius = radius
 
     def area(self):
+        """Returns the area of a circle"""
         return 3.14 * (self.radius * self.radius)
 
     def perimeter(self):
+        """Returns the perimeter of a circle"""
         return 2 * pi * self.radius
 
 
 class Rectangle(Shape):
+    """Rectangle class that inherits from Shape abstract class"""
     def __init__(self, width, height):
+        """Rectancle construct"""
         self.width = width
         self.height = height
 
     def area(self):
+        """Returns the area of a rectangle"""
         return self.width * self.height
 
     def perimeter(self):
+        """Returns the perimeter of a rectangle"""
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
+    """Function to print the area and the perimeter of a shape
+
+    Arguments:
+        shape (Shape class): shape with the methods area() and perimeter()
+    """
     print(f"Area: {shape.area()}")
     print(f"Perimeter: {shape.perimeter()}")
