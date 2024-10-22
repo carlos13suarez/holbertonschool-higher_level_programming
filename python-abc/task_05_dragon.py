0baaff8d4fc473d0fa4da7ede07c6e9bc1a3f9ba
carlos13suarez <charliesuarez10@gmail.com>
2024-10-21 20:13:06 -0500

    task 5

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+class SwimMixin:
+
+    def swim(self):
+        print("The creature swims!")
+
+class FlyMixin:
+
+    def fly(self):
+        print("The creature flies!")
+
+class Dragon(SwimMixin, FlyMixin):
+
+    def roar(self):
+        print("The dragon roars!")
+
+    def scratch(self):
+        print("The dragon scratches!")
