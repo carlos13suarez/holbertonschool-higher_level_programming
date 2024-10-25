7a5fc617af0735af913270883d227965265e30b5
carlos13suarez <charliesuarez10@gmail.com>
2024-10-24 20:42:46 -0500

    task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module that contains a function that creates an Object from a JSON file
+"""
+
+import json
+
+def load_from_json_file(filename):
+    with open(filename) as file:
+        return json.load(file)
