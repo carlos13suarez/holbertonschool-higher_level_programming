1d6cf147a1f0cf67c7246af8908dac889a062540
carlos13suarez <charliesuarez10@gmail.com>
2024-10-24 19:51:19 -0500

    task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains a function to read a text file UTF8
+"""
+
+def read_file(filename=""):
+    """
+    Function that reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename: file to read and print its content
+    """
+    with open(filename) as file:
+        content = file.read()
+        print(content)
