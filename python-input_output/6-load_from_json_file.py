b10a2abf2faec16eca61811fdd362a0e4ee29ca8
carlos13suarez <charliesuarez10@gmail.com>
2024-10-24 20:46:36 -0500

    task 6 pycodestyle

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,6 +5,7 @@ Module that contains a function that creates an Object from a JSON file
 
 import json
 
+
 def load_from_json_file(filename):
     """
     Function that creates an Object from a “JSON file”
