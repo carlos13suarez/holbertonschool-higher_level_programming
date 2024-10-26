50c38d275fa2e9525b449515fc66d799cb48b6e1
carlos13suarez <charliesuarez10@gmail.com>
2024-10-25 21:00:07 -0500

    task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/pyhon3
+"""
+Module contains Student class
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor method
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Public method that returns the dictionary description with
+        simple data structure (list, dictionary, string, integer and
+        boolean) for JSON serialization of an object.
+        """
+        return self.__dict__
