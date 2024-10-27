89924b82c2c698da36304f6bdeec67634f33fef7
carlos13suarez <charliesuarez10@gmail.com>
2024-10-27 13:23:30 -0500

    task 0 fix 0: second parameter in dump

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """
+Module contains two functions to serialize a Python dictionary to a
+JSON file and deserialize the JSON file to recreate the Python Dictionary.
 """
 
 import json
@@ -8,7 +10,7 @@ import json
 def serialize_and_save_to_file(data, filename):
     # Your code here to serialize and save data to the specified file
     with open(filename, 'w') as file:
-        json.dump(data, filename)
+        json.dump(data, file)
 
 def load_and_deserialize(filename):
     # Your code here to load and deserialize data from the specified file
