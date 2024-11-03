95739ee175d3a9ab93e27845ab15b70e90dcf119
carlos13suarez <charliesuarez10@gmail.com>
2024-11-03 14:23:38 -0500

    task 4 fix

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -35,7 +35,7 @@ def add_user():
 
     username = data["username"]
     if username in users:
-        return jsonify({"error": "User already exists"}), 400
+        return jsonify({"error": "Username already exists"}), 400
 
     users[username] = {
         "username": username,
