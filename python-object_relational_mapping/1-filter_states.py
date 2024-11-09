71bc3534dc2dd560eb30826314cdcfbcc1df292b
carlos13suarez <charliesuarez10@gmail.com>
2024-11-09 09:58:28 -0500

    task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa in ascending order by id
+with a name starting with N (upper N)
+
+Usage:
+    ./0-select_states.py <mysql_username> <mysql_password> <database_name>
+
+Arguments:
+    mysql_username: MySQL username
+    mysql_password: MySQL password
+    database_name: Database name
+
+Description:
+    This script connects to a MySQL server running on localhost at port 3306,
+    executes a query to retrieve all states ordered by `id`, and prints each
+    state as a tuple in the format `(id, 'state_name')`.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """
+    Main entrance
+    """
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
