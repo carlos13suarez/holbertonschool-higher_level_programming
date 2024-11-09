850b9604bd44b743f4d53d64f491d35b889d4237
carlos13suarez <charliesuarez10@gmail.com>
2024-11-09 09:51:03 -0500

    task 0 fix 2: module docstrings

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa in ascending order by id
+
+Usage:
+    ./0-select_states.py <mysql_username> <mysql_password> <database_name>
+
+Arguments:
+    mysql_username: MySQL username
+    mysql_password: MySQL password
+    database_name: Database name
+
+Description:
+    This script connects to a MySQL server running on localhost at port 3306,
+    executes a query to retrieve all states ordered by `id`, and prints each
+    state as a tuple in the format `(id, 'state_name')`.
+"""
 
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    """Main entrance"""
+    """
+    Main entrance
+    """
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -13,8 +32,10 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
+
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
+
     rows = cur.fetchall()
     for row in rows:
         print(row)
