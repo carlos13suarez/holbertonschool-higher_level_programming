bc2e2daa4820babd04e2d4d5f204ff46bfdb32d3
carlos13suarez <charliesuarez10@gmail.com>
2024-11-10 17:26:25 -0500

    task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- list all cities in the database hbtn_0d_usa by displaying id, name and its states name, sorting the id in ascending order
-SELECT cities.id, cities.name, (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
+SELECT cities.id, cities.name, states.name AS state_name
 FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
