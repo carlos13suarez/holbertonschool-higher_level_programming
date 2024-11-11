31eaa2ff04b023b85ffc9dec0e4be3c54750631d
carlos13suarez <charliesuarez10@gmail.com>
2024-11-10 23:13:36 -0500

    task 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,5 +2,6 @@
 SELECT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter')
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
